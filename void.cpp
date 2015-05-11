fe35c697f3adf7062aa1ac29db6d8c8fe819c0db
JuanPabloGonzalezHuezzo <A01224902@itesm.mx>
2015-05-10 19:33:50 -0500

    Update and rename LICENSE.md to void.cpp

---
+++ void.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void bla(int i){
+  int p;
+i=i-1;
+p=x;
+cout << x << endl;
+
+}
+
+int main(){
+  int x;
+  int y;
+  cin >> x;
+  bla (x);
+  cout << x << endl;
+
+  // How could I retrieve the value y from bla?(because it is local to the void)
+  //Can it be done? 
+
+
+return 0;
+}
